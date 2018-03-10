8d710041e5609a36737db22e4e26e549c7d14dc1
Minoru <u.minor3110@gmail.com>
2018-03-10 21:15:10 +0900

    add store & reducer

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { ConnectedRouter } from 'react-router-redux'
+import createBrowserHistory from 'history/createBrowserHistory'
+
 import './index.css'
 import App from './App'
+import createStore from './createStore'
+
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const history = createBrowserHistory()
+const store = createStore(history)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('root')
+)
 registerServiceWorker()
